573e25bddc1176f410df2375318a1179d64837ac
GIn <kritsana.ch.57@ubu.ac.th>
2018-08-26 01:36:27 +0700

    [FIX] css logo, title banner and content camp (4  6) and image gallery camp 6

--- creative-gh/genfile.js
+++ creative-gh/genfile.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const path = "assets/images/โครงการสืบสานภูมิปัญญา_5/"
+const path = "assets/images/โครงการสืบสานภูมิปัญญา_6/"
 fs.readdir(path, (err, files) => {
   files.forEach(file => {
     console.log(`"${path}${file}",`);
